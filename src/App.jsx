4a652b9466b1603a7578d88fb658733a72545e62
Bishar <bishar995@gmail.com>
2025-03-05 15:17:11 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -169,13 +169,13 @@ function Home() {
   return (
     <div className="min-h-screen bg-gray-100 p-10">
       <h1 className="text-4xl font-bold text-center mb-6">Latest News</h1>
-      <div className="grid grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {news.map((post, index) => (
           <Link
             to={`/news/${post.id}`}
             key={post.id}
             className={`bg-white shadow-lg p-4 rounded-lg hover:shadow-xl transition ${
-              index === 0 ? "col-span-2" : ""
+              index === 0 ? "sm:col-span-2 lg:col-span-2" : ""
             }`}
           >
             <img
